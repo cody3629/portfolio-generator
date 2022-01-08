21f2ce30a724f7e2f9d2ee45ae335865c99074bc
Cody Webb <webbcody36@gmail.com>
2022-01-07 23:42:01 -0500

    questions

--- app.js
+++ app.js
@@ -1,21 +1,145 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+const promptUser = () => {
 
-console.log(profileDataArgs);
+return inquirer
+.prompt([
+  {
+    type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username',
+      validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your Github username!');
+            return false;
+          }
+        }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
+    }
+  ]);
+};
 
-const [name, github] = profileDataArgs;
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
 
-console.log(name, github);
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
 
-const pageHTML = generatePage(name, github);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project description!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project Github link!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+.then(projectData => {
+  portfolioData.projects.push(projectData);
+  if (projectData.confirmAddProject) {
+    return promptProject(portfolioData);
+  } else {
+    return portfolioData;
+    }
+  });
+};
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+  // if (err) throw err;
+
+  // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 // const printProfileData = profileDataArr => {
     // This...
    // for (let i = 0; i < profileDataArr.length; i+= 1) {
@@ -30,3 +154,4 @@ fs.writeFile('./index.html', pageHTML, err => {
 // };
 
 // printProfileData(profileDataArgs);
+});
